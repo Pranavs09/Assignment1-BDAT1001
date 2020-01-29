f5de8606169c0d114d29908e7120dc4012573b8f
Pranav Sharma <Pranav Sharma@DESKTOP-7A2984A>
2020-01-28 22:20:50 -0500

    initial commit

---
+++ AssignmentIES/Models/Encrypter.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+namespace AssignmentIES.Models
+{
+    class Encrypter
+    {
+        public Encrypter(string originalText, int[] encryptionCipher = null, int encryptionDepth = 1)
+        {
+            OriginalText = originalText;
+
+            if (encryptionCipher != null)
+            {
+                if (EncryptionCipher != encryptionCipher)
+                {
+                    EncryptionCipher = encryptionCipher;
+                }
+            }
+
+            if (EncryptionDepth != encryptionDepth)
+            {
+                EncryptionDepth = encryptionDepth;
+            }
+
+            ConvertText(OriginalText, EncryptionCipher, EncryptionDepth);
+        }
+
+        public void ConvertText(string originalText, int[] encryptionCipher = null, int encryptionDepth = 1)
+        {
+            CipherEncrypted = DeepEncryptWithCipher(OriginalText, EncryptionCipher, EncryptionDepth);
+
+            Base64 = StringToBase64(OriginalText);
+            Binary = StringToBinary(OriginalText);
+            Hexadecimal = StringToHex(OriginalText);
+        }
+
+        public string OriginalText { get; internal set; }
+        public int[] EncryptionCipher { get; } = new[] { 1, 1, 2, 3, 5, 8 }; 
+        public int EncryptionDepth { get; } = 1;
+        public string CipherEncrypted { get; internal set; }
+        public string Base64 { get; internal set; }
+        public string Binary { get; internal set; }
+        public string Hexadecimal { get; internal set; }
+
+        public static string DeepEncryptWithCipher(string originalText, int[] encryptionCipher, int encryptionDepth)
+        {
+            string result = originalText;
+
+            for (int depth = 0; depth < encryptionDepth; depth++)
+            {
+                result = EncryptWithCipher(result, encryptionCipher);
+            }
+
+            return result;
+        }
+        public static string EncryptWithCipher(string text, int[] encryptionCipher)
+        {
+            if (encryptionCipher == null || encryptionCipher.Length == 0)
+            {
+                return text;
+            }
+            byte[] bytearray = Encoding.Unicode.GetBytes(text);
+
+            byte[] bytearrayresult = bytearray;
+
+            int encryptionCipherIndex = 0;
+
+            for (int i = 0; i < bytearray.Length; i++)
+            {
+                encryptionCipherIndex = i;
+
+                if (encryptionCipherIndex >= encryptionCipher.Length)
+                {
+                    encryptionCipherIndex = 0;
+                }
+
+                if (bytearray[i] != 0)
+                {
+                    bytearrayresult[i] = (byte)(bytearray[i] + encryptionCipher[encryptionCipherIndex]);
+                }
+            }
+
+            string newresult = Encoding.Unicode.GetString(bytearrayresult);
+
+            return newresult;
+        }
+
+       
+        public static string DeepDecryptWithCipher(string originalText, int[] encryptionCipher, int encryptionDepth)
+        {
+            string result = originalText;
+
+            string[] encryptedValues = new string[encryptionDepth + 1];
+            encryptedValues[0] = result;
+
+            for (int depth = 0; depth < encryptionDepth; depth++)
+            {
+                result = DecryptWithCipher(result, encryptionCipher);
+
+                encryptedValues[depth + 1] = result;
+            }
+
+            return result;
+        }
+        public static string DecryptWithCipher(string text, int[] encryptionCipher)
+        {
+            byte[] bytearray = Encoding.Unicode.GetBytes(text);
+            byte[] bytearrayresult = bytearray;
+
+            int encryptionCipherIndex = 0;
+
+            for (int i = 0; i < bytearray.Length; i++)
+            {
+                encryptionCipherIndex = i;
+
+                if (encryptionCipherIndex >= encryptionCipher.Length)
+                {
+                    encryptionCipherIndex = 0;
+                }
+
+                if (bytearray[i] != 0)
+                {
+                    bytearrayresult[i] = (byte)(bytearray[i] - encryptionCipher[encryptionCipherIndex]);
+                }
+            }
+
+            string newresult = Encoding.Unicode.GetString(bytearrayresult);
+
+            return newresult;
+        }
+        public static string Base64ToString(string data)
+        {
+            if (String.IsNullOrEmpty(data))
+            {
+                return String.Empty;
+            }
+
+            byte[] bytearray = Convert.FromBase64String(data);
+
+            return Encoding.Unicode.GetString(bytearray);
+        }
+        public static string StringToBase64(string data)
+        {
+            byte[] bytearray = Encoding.Unicode.GetBytes(data);
+
+            return Convert.ToBase64String(bytearray);
+        }
+        public static string StringToHex(string data)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (char c in data.ToCharArray())
+            {
+                sb.Append(Convert.ToString(c, 16).PadLeft(2, '0') + " ");
+            }
+
+            return sb.ToString().ToUpper();
+        }
+        public static string StringToBinary(string data)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (char c in data.ToCharArray())
+            {
+                sb.Append(Convert.ToString(c, 2).PadLeft(8, '0') + " ");
+            }
+            return sb.ToString();
+        }
+
+    }
+}
+
---
+++ AssignmentIES/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using AssignmentIES.Models;
+
+namespace AssignmentIES
+{
+    class StringConversion
+    {
+        public static void Main(string[] args)
+        {
+            // String To Binary
+            Console.WriteLine("Enter Your Name : ");
+            string name = Console.ReadLine();
+            string toBinary = StringToBinary(name);
+            Console.WriteLine(toBinary);
+           
+            //Binary To String
+            Console.WriteLine("Enter Binary Number of Your Name As Same From The Above Code : ");
+            string binary = Console.ReadLine();
+            string toString = BinaryToString(binary);
+            Console.WriteLine(toString);
+            
+            // String To Hexadecimal
+            Console.WriteLine("Enter Your Name : ");
+            string Name = Console.ReadLine();
+            string toHexa = StringToHexa(Name);
+            Console.WriteLine(toHexa);
+
+            //Hexadecimal TO String
+            Console.WriteLine("Enter Your Hexa Decimal Value : ");
+            string Hd = Console.ReadLine();
+            string ToASCII = HexaToAscii(Hd);
+            Console.WriteLine(ToASCII);
+
+            //String To Base64
+            Console.WriteLine("Enter Your Name : ");
+            string name1 = Console.ReadLine();
+            string toBase64 = StringToBase64(name1);
+            Console.WriteLine(toBase64);
+
+            //String To Base64
+            Console.WriteLine("Enter Your Base64 Type : ");
+            string base64 = Console.ReadLine();
+            string toAscii1 = Base64ToString(base64);
+            Console.WriteLine(toAscii1);
+
+            // Encryption and Deceyption 
+            Console.WriteLine("Enter Your Name : ");
+            string unicodeString = Console.ReadLine();
+            int[] cipher = new[] { 1, 1, 2, 3, 5, 8, 13 };
+            string cipherasString = String.Join(",", cipher.Select(x => x.ToString()));
+
+            int encryptionDepth = 20;
+
+            Encrypter encrypter = new Encrypter(unicodeString, cipher, encryptionDepth);
+
+            //Deep Encrytion
+            string nameDeepEncryptWithCipher = Encrypter.DeepEncryptWithCipher(unicodeString, cipher, encryptionDepth);
+            Console.WriteLine($"Deep Encrypted {encryptionDepth} times using the cipher {nameDeepEncryptWithCipher}");
+
+            string nameDeepDecryptWithCipher = Encrypter.DeepDecryptWithCipher(nameDeepEncryptWithCipher, cipher, encryptionDepth);
+            Console.WriteLine($"Deep Decrypted {encryptionDepth} times using the cipher {nameDeepDecryptWithCipher}");
+        }
+
+        // Converting String to Binary
+        public static string StringToBinary(string data)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (char c in data.ToCharArray())
+            {
+                sb.Append(Convert.ToString(c, 2).PadLeft(8, '0'));
+            }
+            return sb.ToString();
+        }
+
+        // Converting Binary to String
+        public static string BinaryToString(string data)
+        {
+            List<Byte> byteList = new List<Byte>();
+
+            for (int i = 0; i < data.Length; i += 8)
+            {
+                byteList.Add(Convert.ToByte(data.Substring(i, 8), 2));
+            }
+            return Encoding.ASCII.GetString(byteList.ToArray());
+        }
+
+        //Converting Your Name To HexaDecimal
+        public static string StringToHexa(string data)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (char c in data.ToCharArray())
+            {
+                sb.Append(Convert.ToString(c, 16).PadLeft(2, '0'));
+            }
+
+            return sb.ToString().ToUpper();
+        }
+
+        // Converting HexaDecimal To ASCII  
+
+        public static string HexaToAscii(string data)
+        {
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i <= data.Length - 2; i += 2)
+            {
+                sb.Append(Convert.ToString(Convert.ToChar(Int32.Parse(data.Substring(i, 2),
+                System.Globalization.NumberStyles.HexNumber))));
+            }
+            return sb.ToString();
+        }
+
+        // String To Base64
+        public static string StringToBase64(string data)
+        {
+            byte[] bytearray = Encoding.ASCII.GetBytes(data);
+
+            string result = Convert.ToBase64String(bytearray);
+
+            return result;
+        }
+
+        // Base64 To Ascii
+        public static string Base64ToString(string base64String)
+        {
+            byte[] bytearray = Convert.FromBase64String(base64String);
+
+            using (var ms = new MemoryStream(bytearray))
+            {
+                using (StreamReader reader = new StreamReader(ms))
+                {
+                    string text = reader.ReadToEnd();
+                    return text;
+                }
+            }
+        }
+    }
+}
+
